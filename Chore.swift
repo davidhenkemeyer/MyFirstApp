20ececb33696dc93ed485d4c37fd92aedc9b0cfb
Ava Henkemeyer <henkemeyer@davids-macbook-pro-2.local>
2018-01-22 15:21:47 -0800

    First commit

---
+++ Chore.swift
@@ -0,0 +1,17 @@
+//
+//  Chore.swift
+//  MyFirstApp
+//
+//  Created by David Henkemeyer on 1/15/18.
+//  Copyright © 2018 David Henkemeyer. All rights reserved.
+//
+
+import Foundation
+
+class Chore {
+    var title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+}
