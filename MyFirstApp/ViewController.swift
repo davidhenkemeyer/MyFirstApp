bc5969d190e090c95a1e165b2d6692ac8d178dde
Ava Henkemeyer <henkemeyer@davids-macbook-pro-2.local>
2018-01-22 15:20:41 -0800

    First Commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
